423e4588b789aa954f9fb692726c38b2bf48ed32
Giles Thomas <giles@giles.net>
2010-06-11 17:20:29 +0100

    Added ExtJS (the whole thing, for now!) and stuff so that it can be accessed from Django dev servers

--- urls.py
+++ urls.py
@@ -1,3 +1,6 @@
+from os.path import join
+
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 from django.contrib import admin
@@ -7,3 +10,22 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^user/', include('wesolver.ui.urls')),
 )
+
+
+if settings.ENVIRONMENT == "dev":
+    urlpatterns += patterns("django.views",
+        url(
+            r"^$",
+            "static.serve",
+            {
+                "document_root" : join(settings.ROOT_DIR, 'dev-site-index'),
+                "path" : "index.html"
+            }
+        ),
+        url(
+            r"ext-js/(?P<path>.*)/$" ,
+            "static.serve",
+            {"document_root": join(settings.ROOT_DIR, 'ext-js')}
+        ),
+    )
+
